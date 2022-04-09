d1b9b59dd41cb3bcac137d127a6b12d932a82443
Dagore <david.gomez.redondo.dev@gmail.com>
2022-04-09 09:11:26 +0200

    Basic echo server behaviour

--- client.js
+++ client.js
@@ -1,6 +1,12 @@
 const { Socket } = require('net')
 const socket = new Socket()
 
+// Para poder leer líneas desde la consola hay que crear una interfaz:
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 const port = 8080
 
 // Se conecta al host y puerto especificados.
@@ -12,10 +18,20 @@ socket.connect({
 // Indicamos la codificación
 socket.setEncoding('utf-8')
 
-// Escribimos algo en el socket
-socket.write('Hola desde el cliente')
+// Leemos una línea:
+readline.on('line', (message) => {
+  // Escribimos algo en el socket
+  socket.write(message)
+
+  if (message === 'END') {
+    socket.end()
+  }
+})
 
 // Recibe algo en este lado del socket
 socket.on('data', (data) => {
   console.log(data)
 })
+
+// Cuando se recibe la señal de cierre del server cerramos el programa.
+socket.on('close', () => process.exit(0))
--- server.js
+++ server.js
@@ -5,19 +5,22 @@ const port = 8080
 
 // Ejecuta el callback cuando le llega una nueva conexión al servidor.
 server.on('connection', (socket) => {
-  console.log(`New connection on ${socket.remoteAddress}:${socket.remotePort}`)
+  const clientSocket = `${socket.remoteAddress}:${socket.remotePort}`
+  console.log(`New connection on ${clientSocket}`)
 
   // Se indica la codificación que usa la conexión. Así el buffer de bytes se parsea directamente.
   socket.setEncoding('utf8')
 
   // Dispara un callback con información que el cliente escribe en el socket.
-  socket.on('data', (data) => {
-    console.log(data)
-    socket.write('Recibido en el servidor')
-
-    // Cierra el socket.
-    socket.end()
+  socket.on('data', (message) => {
+    if (message !== 'END') {
+      console.log(`${clientSocket} => ${message}`)
+    } else {
+      socket.end()
+    }
   })
+
+  socket.on('close', () => console.log('Closed', clientSocket))
 })
 
 server.listen({
